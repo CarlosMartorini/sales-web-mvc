5cb9edd769bef381b55326f18cf19d581f15a68e
Carlos Alberto Martorini Silveira <c7.silveira@gmail.com>
2020-08-17 11:32:35 -0300

    Create other entities with basic attributes, associations, constructors and custom methods + new migration and update database

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public int MyProperty { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, int myProperty, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            MyProperty = myProperty;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc/Models/SalesStatus.cs
@@ -0,0 +1,6 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class SalesStatus
+    {
+    }
+}
