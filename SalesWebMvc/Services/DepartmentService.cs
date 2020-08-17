c2403503aadcd6e3820f65b882596e4da8087798
Carlos Alberto Martorini Silveira <c7.silveira@gmail.com>
2020-08-17 15:41:30 -0300

    Creating the referential integrity with foreign key and select component for DepartmentId in seller create

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
